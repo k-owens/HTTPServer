a100157abc7b35da3e3312819992e3bfee864d10
Keith Owens <kowens@pekininsurance.com>
2016-07-25 11:16:29 -0500

    Add Interface for requests and replies.

---
+++ HTTP Server.core/IConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Server.core
+{
+    public interface IConnection
+    {
+        void Request();
+        void GetReply(String message);
+    }
+}
