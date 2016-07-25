eca89da58892801fbf1b1772227258d2249e7d0e
Keith Owens <kowens@pekininsurance.com>
2016-07-25 11:20:26 -0500

    Add 200 response.

--- HTTP Server.core/Server.cs
+++ HTTP Server.core/Server.cs
@@ -4,19 +4,45 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using HTTP_Server.core;
 
 namespace HTTPServer.core
 {
     public class Server
     {
+        public bool Running = false;
+
         public bool Start()
         {
-            return true;
+            if (!Running)
+            {
+                Running = true;
+                return true;
+            }
+            return false;
         }
 
-        public void Stop()
+        public bool Stop()
+        {
+            if (Running)
+            {
+                Running = false;
+                return true;
+            }
+            return false;
+        }
+
+        public void RespondWith404(IConnection connection)
         {
             
+        }
+
+        public void RespondWith200(IConnection connection)
+        {
+            connection.GetReply("HTTP/1.1 200 OK\n"
+                + "Content-Type:  text/html\n"
+                + "\n"
+                + "<html><body><h1>Hello World</h1></body></html>");
         }
     }
 }
