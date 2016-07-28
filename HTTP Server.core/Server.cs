ceded0ff07fafe1c6c5df7ff529b467f87f2754c
Keith Owens <kowens@pekininsurance.com>
2016-07-28 10:59:01 -0500

    Add functionality for an echo server.

--- HTTP Server.core/Server.cs
+++ HTTP Server.core/Server.cs
@@ -1,48 +1,71 @@
-﻿using System.Text;
-using HTTP_Server.core;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace HTTPServer.core
 {
     public class Server
     {
         public bool Running = false;
+        private Socket socket;
+        private Socket client;
 
         public bool Start()
         {
-            if (!Running)
-            {
-                Running = true;
-                return true;
-            }
-            return false;
+            if (Running) return false;
+
+            Running = true;
+            Console.WriteLine("Echo server has started.");
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+            var ipEndPoint = new IPEndPoint(ipAddress, 8080);
+            socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            PrepareSocketForConnection(ipEndPoint);
+            return true;
+        }
+
+        private void PrepareSocketForConnection(IPEndPoint ipEndPoint)
+        {
+            socket.Bind(ipEndPoint);
+            Console.WriteLine("Server socket is now bound to an end point.");
+            socket.Listen(100);
+            Console.WriteLine("Server has started listening.");
+        }
+
+        public void ConnectToClient()
+        {
+            client = socket.Accept();
+            Console.WriteLine("Server has connected to the client.");
+        }
+
+        public void HandleData()
+        {
+            byte[] buffer = new byte[1024];
+
+            ReceiveData(buffer);
+            SendData(buffer);
+            client.Close();
+        }
+
+        private void SendData(byte[] buffer)
+        {
+            client.Send(buffer);
+            Console.WriteLine("Server is sending data back.");
+        }
+
+        private void ReceiveData(byte[] buffer)
+        {
+            client.Receive(buffer);
+            Console.WriteLine("Server has received data.");
         }
 
         public bool Stop()
         {
-            if (Running)
-            {
-                Running = false;
-                return true;
-            }
-            return false;
-        }
+            if (!Running) return false;
 
-        public int RespondWith404(IConnection connection)
-        {
-            byte[] buffer = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n"
-                         + "Content-Type:  text/html\r\n"
-                         + "\r\n"
-                         + "<html><body><h1>The requested page was not found.</h1></body></html>");
-            return connection.SendReply(buffer);
-        }
-
-        public int RespondWith200(IConnection connection)
-        {
-            byte[] buffer = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n"
-                         + "Content-Type:  text/html\r\n"
-                         + "\r\n"
-                         + "<html><body><h1>Hello World</h1></body></html>");
-            return connection.SendReply(buffer);
+            Running = false;
+            socket.Close();
+            Console.WriteLine("Server has stopped.");
+            return true;
         }
     }
 }
